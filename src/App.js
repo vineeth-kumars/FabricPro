544e63b24f7689849bf31e75b268e5522368c8c5
vineeth-kumars <sattinenivineethkumar@gmail.com>
2022-10-16 21:39:17 +0530

    commiting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import Home from './components/Home';
+import ModelDisplayP18 from './components/modelP18/ModelDisplayP18';
+import ModelDisplayP17 from './components/modelP17/ModelDisplayP17';
+import {BrowserRouter as Router , Routes,Route } from "react-router-dom"
+import ModelDisplayP13 from './components/modelP13/ModelDisplayP13';
+import ModelDisplayP4 from './components/modelp4/ModelDisplayP4';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router >
+      <Routes>
+      <Route exact path="/" element={<Home/>} />
+      <Route exact path="/modelP18" element={<ModelDisplayP18/>} />
+      <Route exact path="/modelP17" element={<ModelDisplayP17/>} />
+      <Route exact path="/modelP13" element={<ModelDisplayP13/>} />
+      <Route exact path="/modelP4" element={<ModelDisplayP4/>} />
+      </Routes>
+     
+    </Router>
+    
+   
+   
+    
   );
 }
 
